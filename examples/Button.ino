ae1c33d0d792e0df0d46859057921b1d1b0d91f7
daikw <hazuhi@gmail.com>
2021-02-20 15:52:59 +0900

    :+1: try `Button`

---
+++ examples/Button.ino
@@ -0,0 +1,24 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonState = 0;
+
+void setup()
+{
+    pinMode(buttonPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    buttonState = digitalRead(buttonPin);
+
+    if (buttonState == LOW)
+    {
+        digitalWrite(ledPin, LOW);
+    }
+    else
+    {
+        digitalWrite(ledPin, HIGH);
+    }
+}
