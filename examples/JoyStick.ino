b05e9ff3673ab0caa7d412466d83666aa22a1d19
daikw <hazuhi@gmail.com>
2021-02-23 00:38:01 +0900

    :+1: use joystick's button to suppress outputs

--- examples/JoyStick.ino
+++ examples/JoyStick.ino
@@ -1,17 +1,26 @@
 const PROGMEM int X_PIN = A1;
 const PROGMEM int Y_PIN = A0;
+const PROGMEM int BTN_PIN = 2;
 
 int xValue = 0;
 int yValue = 0;
 
-void setup() { Serial.begin(9600); }
+int buttonState = LOW;
+
+void setup() {
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+}
 
 void loop() {
+  int buttonState = digitalRead(BTN_PIN);
+
   xValue = analogRead(X_PIN);
   yValue = analogRead(Y_PIN);
-  Serial.print(xValue);
+  Serial.print(xValue * buttonState);
   Serial.print(",");
-  Serial.print(yValue);
+  Serial.print(yValue * buttonState);
   Serial.println("");
+
   delay(50);
 }
