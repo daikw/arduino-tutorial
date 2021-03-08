a43829f3ef4b8ee78a10456c6b3eae1035bdd7d3
daikw <hazuhi@gmail.com>
2021-03-08 21:37:01 +0900

    :+1: the alternate button with blinking led

--- examples/Timer.ino
+++ examples/Timer.ino
@@ -1,5 +1,7 @@
 #include <MsTimer2.h>
-#define led 2
+
+#define button 2
+#define led 3
 
 void flash() {
   static boolean output = HIGH;
@@ -10,9 +12,41 @@ void flash() {
 
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
 
   MsTimer2::set(500, flash);
   MsTimer2::start();
 }
 
-void loop() {}
+void toggleTimer() {
+  static bool state = true;
+
+  state = !state;
+  if (state) {
+    MsTimer2::stop();
+  } else {
+    MsTimer2::start();
+  }
+}
+
+void loop() {
+  static bool lastButtonState = LOW;
+  static bool buttonState = LOW;
+  static unsigned int lastDebounceTime = 0;
+  static unsigned int debounceDelay = 50;
+
+  bool reading = digitalRead(button);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  bool waited = (millis() - lastDebounceTime) < debounceDelay;
+  bool alternate = (reading != buttonState);
+
+  if (waited && alternate) {
+    buttonState = reading;
+    if (buttonState) toggleTimer();
+  }
+
+  lastButtonState = buttonState;
+}
