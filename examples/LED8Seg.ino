f94c7019de338be880986255ff5393aef292fc4c
daikw <hazuhi@gmail.com>
2021-03-12 00:05:03 +0900

    :sparkles: count numbers by using 8seg-led

--- examples/LED8Seg.ino
+++ examples/LED8Seg.ino
@@ -1,13 +1,38 @@
+bool numbers[10][8] = {
+    {1, 0, 1, 1, 1, 1, 0, 1},  // 0
+    {0, 0, 0, 0, 0, 1, 0, 1},  // 1
+    {0, 1, 1, 1, 1, 1, 0, 0},  // 2
+    {0, 1, 1, 0, 1, 1, 0, 1},  // 3
+    {1, 1, 0, 0, 0, 1, 0, 1},  // 4
+    {1, 1, 1, 0, 1, 0, 0, 1},  // 5
+    {1, 1, 1, 1, 1, 0, 0, 1},  // 6
+    {1, 0, 0, 0, 1, 1, 0, 1},  // 7
+    {1, 1, 1, 1, 1, 1, 0, 1},  // 8
+    {1, 1, 1, 0, 1, 1, 0, 1},  // 9
+};
+
 void setup() {
-  for (int i = 2; i <= 9; i++) {
-    pinMode(i, OUTPUT);
+  for (int led = 2; led <= 9; led++) {
+    pinMode(led, OUTPUT);
+  }
+}
+
+void clear() {
+  for (int led = 2; led <= 9; led++) {
+    digitalWrite(led, LOW);
+  }
+}
+
+void display(int num) {
+  for (int led = 2; led <= 9; led++) {
+    digitalWrite(led, numbers[num][led - 2]);
   }
 }
 
 void loop() {
-  for (int i = 2; i <= 9; i++) {
-    digitalWrite(i, HIGH);
-    delay(50);
-    digitalWrite(i, LOW);
+  for (int num = 0; num < 10; num++) {
+    display(num);
+    delay(1000);
+    clear();
   }
 }
