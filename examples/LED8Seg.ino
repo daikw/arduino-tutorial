a16065a0f443cf37e42bf55a6a2700759986efb7
daikw <hazuhi@gmail.com>
2021-03-13 22:30:47 +0900

    :+1: display hex numbers

--- examples/LED8Seg.ino
+++ examples/LED8Seg.ino
@@ -1,7 +1,7 @@
 const int leds[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 const int seg = 8;  // number of the led segment
 
-const bool numbers[10][8] = {
+const bool hex[16][8] = {
     {1, 0, 1, 1, 1, 1, 0, 1},  // 0
     {0, 0, 0, 0, 0, 1, 0, 1},  // 1
     {0, 1, 1, 1, 1, 1, 0, 0},  // 2
@@ -12,9 +12,15 @@ const bool numbers[10][8] = {
     {1, 0, 0, 0, 1, 1, 0, 1},  // 7
     {1, 1, 1, 1, 1, 1, 0, 1},  // 8
     {1, 1, 1, 0, 1, 1, 0, 1},  // 9
+    {1, 1, 0, 1, 1, 1, 1, 1},  // A
+    {1, 1, 1, 1, 1, 1, 1, 1},  // B
+    {1, 0, 1, 1, 1, 0, 1, 0},  // C
+    {1, 0, 1, 1, 1, 1, 1, 1},  // D
+    {1, 1, 1, 1, 1, 0, 1, 0},  // E
+    {1, 1, 0, 1, 1, 0, 1, 0},  // F
 };
 
-const int pi[10] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+// const int pi[10] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
 void setup() {
   for (int i = 0; i < seg; i++) {
@@ -28,15 +34,15 @@ void clear() {
   }
 }
 
-void display(int num) {
+void display(int index) {
   for (int i = 0; i < seg; i++) {
-    digitalWrite(leds[i], numbers[num][i]);
+    digitalWrite(leds[i], hex[index][i]);
   }
 }
 
 void loop() {
-  for (int num = 0; num < 10; num++) {
-    display(pi[num]);
+  for (int num = 0; num < 16; num++) {
+    display(num);
     delay(1000);
     clear();
   }
