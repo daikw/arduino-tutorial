bd864f365c3bd7443ca32cab4467e327d2fa43ac
daikw <hazuhi@gmail.com>
2021-03-20 14:08:23 +0900

    :recycle:

--- examples/ShiftRegister.ino
+++ examples/ShiftRegister.ino
@@ -4,6 +4,9 @@
 //   - https://omoroya.com/arduino-lesson20/
 //   - https://synapse.kyoto/glossary/shift_register/page001.html
 //
+// datasheet:
+//   [253DSN74HC595N](https://www.ti.com/lit/ds/symlink/sn74hc595.pdf?ts=1616216539681&ref_url=https%253A%252F%252Fwww.ti.com%252Fsitesearch%252Fdocs%252Funiversalsearch.tsp%253FsearchTerm%253DSN74HC595N)
+//
 // words:
 //   - D-flipflop ... https://synapse.kyoto/glossary/dff/page001.html
 //   - multiplexer
@@ -16,7 +19,7 @@ int dsPin = 9;      // (14) DS [SER] on 74HC595
 int rclkPin = 11;   // (12) ST_CP [RCLK] on 74HC595
 int srclkPin = 12;  // (11)  SH_CP [SRCLK] on 74HC595
 
-byte leds = 0;
+byte leds = 0b00000000;  // 8 bits
 
 void setup() {
   pinMode(dsPin, OUTPUT);
@@ -34,7 +37,7 @@ void loop() {
     shiftOut(dsPin, srclkPin, LSBFIRST, leds);
     digitalWrite(rclkPin, HIGH);
 
-    delay(200);
+    delay(200);  // tune this to alter the behaviour
 
     bitClear(leds, i);
   }
