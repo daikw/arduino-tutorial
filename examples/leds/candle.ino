ab6d471fb64c089b9730bf4ca6e0195cd54be303
daikw <hazuhi@gmail.com>
2021-04-07 00:08:20 +0900

    :+1: add missing changes

--- examples/leds/candle.ino
+++ examples/leds/candle.ino
@@ -1,11 +1,16 @@
-int led = 2;
+int led = A0;
 
 void setup() { pinMode(led, OUTPUT); }
 
-const int FLUSH_DELAY = 250;
+const int DELAY = 20;
 
 void loop() {
-  for (int i = 0; i < 10; i++) {
-    analogWrite() flush(rand);
+  for (int i = 0; i < 255; i++) {
+    analogWrite(led, i);
+    delay(DELAY);
+  }
+  for (int i = 0; i < 255; i++) {
+    analogWrite(led, 255 - i);
+    delay(DELAY);
   }
 }
