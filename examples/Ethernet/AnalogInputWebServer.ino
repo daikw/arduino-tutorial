647a6adfab1cfd0dfea1411587f2407084ba9ddb
daikw <hazuhi@gmail.com>
2021-04-06 23:29:25 +0900

    :recycle: :books:

--- examples/Ethernet/AnalogInputWebServer.ino
+++ examples/Ethernet/AnalogInputWebServer.ino
@@ -7,41 +7,25 @@
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
  * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
-
  */
 
 #include <Ethernet.h>
 #include <SPI.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0x5D, 0x06};
 IPAddress ip(169, 254, 0, 1);
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
 EthernetServer server(80);
 
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ;  // wait for serial port to connect. Needed for native USB port only
   }
   Serial.println("Ethernet WebServer Example");
 
-  // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
 
-  // Check for Ethernet hardware present
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println(
         "Ethernet shield was not found.  Sorry, can't run without hardware. "
@@ -54,7 +38,6 @@ void setup() {
     Serial.println("Ethernet cable is not connected.");
   }
 
-  // start the server
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
---
+++ examples/Ethernet/memo.md
@@ -0,0 +1,16 @@
+## command
+
+```
+$ watch -n1 "curl http://169.254.0.1 2>/dev/null"
+```
+
+## circuit
+
+voltage dividor circuit with a photoresitor (PH).
+you can find `A0` value changes when you cover the top of PH.
+
+```
+5V           A0       GND
+|            |        |
+|--[1 kohm]--|--[PH]--|
+```
